23fce1582906316adfdc03551c5f751ad18df3d5
Liam Monninger <l.mak.monninger@gmail.com>
2023-12-03 10:06:56 -0800

    chore: structure.

---
+++ target/debug/deps/badlock-abaea4b2790f5247.d
@@ -0,0 +1,5 @@
+/Users/l-monninger/dev/badlock/target/debug/deps/badlock-abaea4b2790f5247.rmeta: badlock/clis/badlock/src/lib.rs
+
+/Users/l-monninger/dev/badlock/target/debug/deps/badlock-abaea4b2790f5247.d: badlock/clis/badlock/src/lib.rs
+
+badlock/clis/badlock/src/lib.rs:
